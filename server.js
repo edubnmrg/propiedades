f173a9b24688a24ec915e6c8f411a6efffdd01c0
edubnmrg <eduardoben@gmail.com>
2016-07-16 17:32:21 -0300

    added dependencies and overhaul of page structure

--- server.js
+++ server.js
@@ -3,6 +3,16 @@ var fs =require(`fs`);
 var request=require(`request`);
 var cheerio=require(`cheerio`);
 var app=express();
+var exphbs  = require('express-handlebars');
+
+app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
+
+app.get('/', function (req, res) {
+    res.render('home');
+});
+
+
 
 app.get(`/scrape`,function(req,res){
   //tt3691740
@@ -38,45 +48,107 @@ app.get(`/scrape`,function(req,res){
   })
 })
 
-app.get(`/orion`,function(req,res){
+app.get(`/zonaprop`,function(req,res){
   //tt3691740
   url = 'http://www.zonaprop.com.ar/propiedades/av-callao-2000-recoleta-capital-federal-31744053.html'
   if(req.query.query_url){
 
     url = req.query.query_url
     request(url, function(error, response, html){
+      console.log(error)
         if(!error){
 
             var $ = cheerio.load(html);
 
-            var fotos, title, release, rating, precio, expensas, descripcion, titulo, datos, imagen;
-
-            var contenedor_begin = "<div class='container' style='margin:-8px;background: #eee'><div class='header' style='height:90px;background:rgba(0,0,0,0.6)' ><img style='position:absolute;top:15px;left:20px;display:none;' width='201' height='75' src='http://orionpropiedades.com/images/logoOrionProp.png'/></div>"
-            var contenedor_end = "</div>"
-
-            var pagina_begin = "<div style='width:80%;background:white;box-shadow:0px 0px 2px #222222;border: 1px solid #e1e1e1;margin:40px auto;padding:40px;'>";
-            var pagina_end = "</div>";
-
-            titulo = "<h1>" + $("h1").text() + "</h1>"
-            precio = "<h2> Precio de Venta: " + $(".venta").text() + "</h2>"
-            descripcion = "<p>" + $("#id-descipcion-aviso").text().trim() + "</p>"
-            datos = "<ul>" + $(".aviso-datos ul").html() + "</ul>";
-            var imagenes_html = "";
-
-            $(".rsMainSlideImage").each(function(i, elem){
-                imagenes_html = imagenes_html +  "<img style='vertical-align:top;display:inline-block; margin: 10px' width=300 src=" + $(elem).attr("href") + ">"
+            var precio, descripcion, titulo, datos;
+            titulo = $("h1").text()
+            precio =$(".venta").text()
+            descripcion =  $("#id-descipcion-aviso").text().trim()
+            datos=[];
+            $(".aviso-datos ul li").each(function(i, elem){
+              datos.push({texto:$(elem).text()})
             })
 
-            texto = contenedor_begin + pagina_begin + titulo + descripcion + precio + datos + imagenes_html + pagina_end + contenedor_end;
+            var imagenes_arr=[];
+            $(".rsMainSlideImage").each(function(i, elem){
+                imagenes_arr.push({imagen:$(elem).attr("href")})
+            })
+            console.error(html.toString())
 
         }
-        //res.send(html)
-        res.send(texto)
+        //console.error(titulo, precio, descripcion, datos, imagenes_arr)
+        res.render('props',{titulo,precio,descripcion,datos,imagenes_arr});
     })
   } else {
-    var style = "width: 300px;height: 40px;line-height: 40px;font-size: 16px;padding: 10px;"
-    res.send("<h1>Pegar url</h1><form method=GET action='/orion'><input style='"+style+"' type='text' name='query_url'></form>")
+    res.render('props_form');
   }
+})
+app.get(`/clarin`,function(req,res){
+  //tt3691740
+  url = 'http://www.clarin.com.ar'
+
+  request(url, function(error, response, html){
+      if(!error){
+
+          var $ = cheerio.load(html);
+
+          var href, title, target;
+
+          var encabezamiento="Titulos Clarin"
+          var titulos_html = "";
+          var titulos = [];
+          var resumen_html=""
+          $("article.nota , article.nota-img-full ").each(function(i, elem){
+              titulos.push({
+                title:$(elem).find("a").attr("title"),
+                description:$(elem).find("p").text()
+              });
+              // fs.open('./my_file.txt', 'a', function opened(err, fd) {
+              // if (err) { throw err; }
+              // var writeBuffer = new Buffer($(elem).find("a").attr("title") + " " + $(elem).find("p").text()),
+              // bufferPosition = 0,
+              // bufferLength = writeBuffer.length, filePosition = null;
+              // fs.write( fd,
+              // writeBuffer,
+              // bufferPosition,
+              // bufferLength,
+              // filePosition,
+              // function wrote(err, written) {
+              // if (err) { throw err; }
+              // console.log('wrote ' + written + ' bytes');
+              // });
+              // });
+          })
+
+
+
+          // var sqlite3 = require('sqlite3').verbose();
+          // var db = new sqlite3.Database('mydb.db');
+          // var check;
+          // db.serialize(function() {
+          //
+          //   db.run("CREATE TABLE if not exists clarin (id INTEGER PRIMARY KEY ASC,titulo TEXT,descripcion TEXT)");
+          //   var stmt = db.prepare("INSERT INTO clarin VALUES (?,?)");
+          //   console.log("antes");
+          //   titulos.forEach(function(ele){
+          //     console.log(ele.title);
+          //     stmt.run(ele.title,ele.description);
+          //   });
+          //
+          //   stmt.finalize();
+          //   console.log("desupes");
+          //   db.each("SELECT rowid AS id, titulo,descripcion FROM clarin", function(err, row) {
+          //       console.log(row.id + ": " + row.titulo+" "+row.descripcion);
+          //   });
+          // });
+          //
+          // db.close();
+
+      }
+      //res.send(html)
+      //res.send(texto)
+      res.render('clarin',{encabezamiento,titulos});
+    })
 })
 
 
