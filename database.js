7cd982a95c4e9bc38759d73fa114ca50de23898b
edubnmrg <eduardoben@gmail.com>
2016-07-21 18:10:20 -0300

    filedatabase

---
+++ database.js
@@ -0,0 +1,46 @@
+var fs =require(`fs`);
+///leer linea
+///escribir un item
+///modificar registro
+
+function Database(){}
+
+Database.prototype.read = function(callback){
+  var visitas = [];
+  var lineReader = require('readline').createInterface({
+    input: require('fs').createReadStream('./historial.txt')
+  });
+
+  lineReader.on('line', function (line) {
+    var partes=[];
+    partes=line.split(",");
+    console.log(partes[0]+partes[1]);
+    visitas.push({texto: partes[0],numero: partes[1]});
+  });
+  lineReader.on('close',function(){
+    callback(visitas)
+  });
+  return true;
+}
+Database.prototype.write = function(item){
+  var fs = require('fs');
+  fs.appendFile("./historial.txt", (item.prop + "," + item.numero), function (err) {
+
+
+    console.log("The item was saved!");
+  });
+}
+
+//[string, numero]
+//registro[1] = registro[1] + 1;
+
+Database.prototype.flush = function(){
+
+}
+var db = new Database();
+
+db.read(function(res){ console.log(res)})
+db.write({ prop: "cabildo", numero: "5"})
+db.read(function(res){ console.log(res)})
+
+module.exports.instance = new Database();
--- server.js
+++ server.js
@@ -4,154 +4,114 @@ var request=require(`request`);
 var cheerio=require(`cheerio`);
 var app=express();
 var exphbs  = require('express-handlebars');
+var auth = require('basic-auth');
+var cookieParser = require('cookie-parser');
+var database = require('./database').instance;
 
+console.log(database.read(function(visitas){ console.log(visitas)}));
+
+app.use(cookieParser());
 app.engine('handlebars', exphbs({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
 
-app.get('/', function (req, res) {
-    res.render('home');
+var basicAuth = require('basic-auth');
+VALID_USER = "AGENTE"
+VALID_PASSWORD = "ORION"
+
+var auth = function (req, res, next) {
+  function unauthorized(res) {
+    res.set('WWW-Authenticate', 'Basic realm=Authorization Required');
+    return res.send(401);
+  };
+
+  var user = basicAuth(req);
+
+  if (!user || !user.name || !user.pass) {
+    return unauthorized(res);
+  };
+
+  if (user.name === VALID_USER && user.pass === VALID_PASSWORD) {
+    return next();
+  } else {
+    return unauthorized(res);
+  };
+};
+
+app.get(`/`,auth,function(req,res){
+
+  res.cookie("agente", true);
+  database.read(function(visitas){
+    res.render('props_form',{visitas});
+  })
+
 });
 
+  app.get(`/props`,function(req,res){
+    if(req.query.query_url){
+      url = req.query.query_url
+      request(url, function(error, response, html){
 
-
-app.get(`/scrape`,function(req,res){
-  //tt3691740
-  var url = 'http://www.imdb.com/title/' + req.query.movie_id;
-  var movie_json = {}
-  request(url, function(error, response, html){
-    if(!error){
-            // Next, we'll utilize the cheerio library on the returned html which will essentially give us jQuery functionality
-
-            var $ = cheerio.load(html);
-
-            // Finally, we'll define the variables we're going to capture
-
-            var title, release, rating;
-            var movie_json = { title : "", release : "", rating : ""};
-            $('.title_wrapper h1').filter(function(){
-                var data = $(this);
-                movie_json.title = $(this).text();
-                //movie_json.release = release;
-            })
-            $('.star-box-giga-star').filter(function(){
-                var data = $(this);
-
-                // The .star-box-giga-star class was exactly where we wanted it to be.
-                // To get the rating, we can simply just get the .text(), no need to traverse the DOM any further
-
-                rating = data.text();
-
-                movie_json.rating = rating;
-            })
-        }
-        res.send('Finished Request: ' + JSON.stringify(movie_json) + '. If its empty, we have failed to find the desired information.')
-  })
-})
-
-app.get(`/zonaprop`,function(req,res){
-  //tt3691740
-  url = 'http://www.zonaprop.com.ar/propiedades/av-callao-2000-recoleta-capital-federal-31744053.html'
-  if(req.query.query_url){
-
-    url = req.query.query_url
-    request(url, function(error, response, html){
-      console.log(error)
-        if(!error){
-
-            var $ = cheerio.load(html);
-
-            var precio, descripcion, titulo, datos;
-            titulo = $("h1").text()
-            precio =$(".venta").text()
-            descripcion =  $("#id-descipcion-aviso").text().trim()
-            datos=[];
-            $(".aviso-datos ul li").each(function(i, elem){
-              datos.push({texto:$(elem).text()})
+          if(!error){
+            database.read(function(visitas){
+              //find record
+              //if not record: database.write(record)
+              //if not record:
             })
 
-            var imagenes_arr=[];
-            $(".rsMainSlideImage").each(function(i, elem){
-                imagenes_arr.push({imagen:$(elem).attr("href")})
-            })
-            console.error(html.toString())
+            var found = false;
+            console.log("dimension "+visitas.length);
+            console.log(url);
+            for (var i = 0; i < visitas.length && !found; i++) {
+              if (visitas[i] === url) {
+                visitas[i].numero++
+                found = true;
+              }
+            }
+            if(!found)  {
+              database.write('./historial.txt',(url+","+"0"+"\n"),function(err){
+                           if (err) { throw err; }
+                           console.log('wrote ' + written + ' bytes');
+                         });
+              };
+            }else{
+                fs.unlinkSync('./historial.txt');
+                 for (var i = 0, len = visitas.length; i < len; i++) {
+                   database.write('./historial.txt',visitas[i].texto+","+visitas[i].numero+"\n",function(err){
+                                if (err) { throw err; }
+                                console.log('wrote ' + written + ' bytes');
+                              });
 
-        }
-        //console.error(titulo, precio, descripcion, datos, imagenes_arr)
-        res.render('props',{titulo,precio,descripcion,datos,imagenes_arr});
-    })
-  } else {
-    res.render('props_form');
-  }
-})
-app.get(`/clarin`,function(req,res){
-  //tt3691740
-  url = 'http://www.clarin.com.ar'
-
-  request(url, function(error, response, html){
-      if(!error){
-
-          var $ = cheerio.load(html);
-
-          var href, title, target;
-
-          var encabezamiento="Titulos Clarin"
-          var titulos_html = "";
-          var titulos = [];
-          var resumen_html=""
-          $("article.nota , article.nota-img-full ").each(function(i, elem){
-              titulos.push({
-                title:$(elem).find("a").attr("title"),
-                description:$(elem).find("p").text()
-              });
-              // fs.open('./my_file.txt', 'a', function opened(err, fd) {
-              // if (err) { throw err; }
-              // var writeBuffer = new Buffer($(elem).find("a").attr("title") + " " + $(elem).find("p").text()),
-              // bufferPosition = 0,
-              // bufferLength = writeBuffer.length, filePosition = null;
-              // fs.write( fd,
-              // writeBuffer,
-              // bufferPosition,
-              // bufferLength,
-              // filePosition,
-              // function wrote(err, written) {
-              // if (err) { throw err; }
-              // console.log('wrote ' + written + ' bytes');
-              // });
-              // });
-          })
+                 };
 
 
+            }
 
-          // var sqlite3 = require('sqlite3').verbose();
-          // var db = new sqlite3.Database('mydb.db');
-          // var check;
-          // db.serialize(function() {
-          //
-          //   db.run("CREATE TABLE if not exists clarin (id INTEGER PRIMARY KEY ASC,titulo TEXT,descripcion TEXT)");
-          //   var stmt = db.prepare("INSERT INTO clarin VALUES (?,?)");
-          //   console.log("antes");
-          //   titulos.forEach(function(ele){
-          //     console.log(ele.title);
-          //     stmt.run(ele.title,ele.description);
-          //   });
-          //
-          //   stmt.finalize();
-          //   console.log("desupes");
-          //   db.each("SELECT rowid AS id, titulo,descripcion FROM clarin", function(err, row) {
-          //       console.log(row.id + ": " + row.titulo+" "+row.descripcion);
-          //   });
-          // });
-          //
-          // db.close();
+              var $ = cheerio.load(html);
 
+              var precio, descripcion, titulo, datos;
+              titulo = $("h1").text()
+              precio =$(".venta").text()
+              descripcion =  $("#id-descipcion-aviso").text().trim()
+              datos=[];
+              $(".aviso-datos ul li").each(function(i, elem){
+                datos.push({texto:$(elem).text()})
+              })
+
+              var imagenes_arr=[];
+              $(".rsMainSlideImage").each(function(i, elem){
+                  imagenes_arr.push({imagen:$(elem).attr("href")})
+              })
+
+          });
+          //console.error(titulo, precio, descripcion, datos, imagenes_arr)
+          var agent = (req.cookies.agente == "true")
+          res.render('props',{titulo,precio,descripcion,datos,imagenes_arr,agent});
       }
-      //res.send(html)
-      //res.send(texto)
-      res.render('clarin',{encabezamiento,titulos});
-    })
-})
-
+    });
+  // app.post('/boton', function(sReq, sRes){
+  //   console.log(sReq.query.cliente);
+  // });
 
 app.listen(`8081`);
-console.log(`magic happens on port 8081`);
+console.log(`Server is up and running`);
 exports=module.exports=app;
